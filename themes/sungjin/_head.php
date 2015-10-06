acda5158ba42f4c49efa927003b2fda7fe08ae11
Charles Koh <charles.jcko@gmail.com>
2015-10-06 23:29:48 +0900

    게시판 상세화면 수정 중

--- themes/sungjin/_head.php
+++ themes/sungjin/_head.php
@@ -6,6 +6,9 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 include_once($themes_path.'/head.php');
 
 ?>
+<div id="logo">
+    <img src="../assets/img/logo.png">
+</div>
 <div id="board_line_01"></div>
 <div id="board_line_02"></div>
 <div id="board_line_03"></div>
