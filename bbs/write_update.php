1deed12ce912fb03d37feea8e85d3033de5c800d
Charles Koh <charles.jcko@gmail.com>
2015-10-24 23:20:57 +0900

    파일업로드 오류 수정

--- bbs/write_update.php
+++ bbs/write_update.php
@@ -177,6 +177,30 @@ if ($w == '' || $w == 'r') {
 if (!isset($_POST['wr_subject']) || !trim($_POST['wr_subject']))
     alert('제목을 입력하여 주십시오.');
 
+// uploads디렉토리에 파일을 업로드합니다. 
+$uploaddir = '/var/www/ams/video/'; 
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']); 
+
+if($_POST['MAX_FILE_SIZE'] < $_FILES['userfile']['size']){ 
+    alert("업로드 파일이 지정된 파일크기보다 큽니다."); 
+} else { 
+    if(($_FILES['userfile']['error'] > 0) || ($_FILES['userfile']['size'] <= 0)){ 
+        alert("파일 업로드에 실패하였습니다.: ".$_FILES['userfile']['error']); 
+    } else { 
+        // HTTP post로 전송된 것인지 체크합니다. 
+        if(!is_uploaded_file($_FILES['userfile']['tmp_name'])) { 
+            alert("HTTP로 전송된 파일이 아닙니다."); 
+        } else { 
+            // move_uploaded_file은 임시 저장되어 있는 파일을 ./uploads 디렉토리로 이동합니다. 
+            if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) { 
+                alert("성공적으로 업로드 되었습니다."); 
+            } else { 
+                alert("파일 업로드 실패입니다."); 
+            } 
+        } 
+    } 
+} 
+
 if ($w == '' || $w == 'r') {
 
     if ($member['mb_id']) {
