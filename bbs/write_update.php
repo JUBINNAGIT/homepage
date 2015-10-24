9b582b4a281069bb8995844b100e408203590d6f
Charles Koh <charles.jcko@gmail.com>
2015-10-24 23:24:11 +0900

    파일업로드 추가오류 수정

--- bbs/write_update.php
+++ bbs/write_update.php
@@ -192,9 +192,7 @@ if($_POST['MAX_FILE_SIZE'] < $_FILES['userfile']['size']){
             alert("HTTP로 전송된 파일이 아닙니다."); 
         } else { 
             // move_uploaded_file은 임시 저장되어 있는 파일을 ./uploads 디렉토리로 이동합니다. 
-            if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) { 
-                alert("성공적으로 업로드 되었습니다."); 
-            } else { 
+            if (!move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) { 
                 alert("파일 업로드 실패입니다."); 
             } 
         } 
