c8dc476ef48914044ef83d0ba5f9fb5640b2ce1f
jkoh <jkoh@hist.co.kr>
2018-03-28 17:15:17 +0900

    게시판 html 테스트

--- bbs/write_update.php
+++ bbs/write_update.php
@@ -343,26 +343,26 @@ if ($w == '' || $w == 'r') {
     $option = ($option == '') ? $mail : $option + ',' + $mail;
 
     $sql = " update {$write_table}
-                set ca_name = '{$ca_name}',
-                     wr_option = '{$option}',
-                     wr_subject = '{$wr_subject}',
-                     wr_content = '{$wr_content}',
-                     wr_link1 = '{$wr_link1}',
-                     wr_link2 = '{$wr_link2}',
-                     mb_id = '{$mb_id}',
-                     wr_name = '{$wr_name}',
-                     wr_email = '{$wr_email}',
-                     wr_homepage = '{$wr_homepage}',
-                     wr_1 = '{$wr_1}',
-                     wr_2 = '{$wr_2}',
-                     wr_3 = '{$wr_3}',
-                     wr_4 = '{$wr_4}',
-                     wr_5 = '{$wr_5}',
-                     wr_6 = '{$wr_6}',
-                     wr_7 = '{$wr_7}',
-                     wr_8 = '{$wr_8}',
-                     wr_9 = '{$wr_9}',
-                     wr_10= '{$wr_10}'
+                set ca_name = '$ca_name',
+                     wr_option = '$option',
+                     wr_subject = '$wr_subject',
+                     wr_content = '$wr_content',
+                     wr_link1 = '$wr_link1',
+                     wr_link2 = '$wr_link2',
+                     mb_id = '$mb_id',
+                     wr_name = '$wr_name',
+                     wr_email = '$wr_email',
+                     wr_homepage = '$wr_homepage',
+                     wr_1 = '$wr_1',
+                     wr_2 = '$wr_2',
+                     wr_3 = '$wr_3',
+                     wr_4 = '$wr_4',
+                     wr_5 = '$wr_5',
+                     wr_6 = '$wr_6',
+                     wr_7 = '$wr_7',
+                     wr_8 = '$wr_8',
+                     wr_9 = '$wr_9',
+                     wr_10= '$wr_10'
                      {$sql_ip}
                      {$sql_password}
               where wr_id = '{$wr['wr_id']}' ";
